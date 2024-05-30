25a4e9b7e70ab7818b5eec4b321bd05ed5ad88e8
viniciusminas <140345005+viniciusminas@users.noreply.github.com>
2024-05-30 19:07:41 -0300

    Projeto Teste para realização de exercicios

---
+++ ProjetoTeste/src/br/com/viniciusminas/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.viniciusminas.git;
+
+/**
+ * @author Vinícius Minas
+ * Exemplo de Integração com do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+		
+	}
+
+}
